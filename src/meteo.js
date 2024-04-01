d9ad7a190c9109d43197a01b2f309003db5814ce
Cyril Freeman <cyril.freeman@hotmail.fr>
2024-04-01 11:34:54 +0200

    traduct data in french, the results are good

--- src/meteo.js
+++ src/meteo.js
@@ -1,5 +1,8 @@
 const fs = require("fs");
 
+// const loader = document.querySelector(".loader-container");
+// const errorInformation = document.querySelector(".error-information");
+
 // Read city from conf.json
 fs.readFile("conf.json", "utf8", (err, data) => {
   if (err) {
@@ -11,21 +14,28 @@ fs.readFile("conf.json", "utf8", (err, data) => {
   // Fetch weather data from OpenWeatherMap API
   const apiKey = "523d773f11d9f264c85fc0c42f94ddf3"; // replace with your actual API key
   fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+    `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&lang=fr&units=metric`
   )
     .then((response) => response.json())
     .then((data) => {
-      console.log(`Weather in ${city}: ${data.weather[0].main}`); // Log weather data
-
+      console.log(`Weather in ${city}: ${data.weather[0].main} `); // Log weather data
+      console.log(`Weather in ${city}: ${data.weather[0].description}`); // decritpion de la meteo
+      console.log(`Weather in ${city}: ${data.main.temp} `); // temperature en kelvin
+      console.log(`Weather in ${city}: ${data.wind.speed}`);
+      console.log(data.weather[0].icon);
       // Update weather data every hour
       setInterval(() => {
         fetch(
-          `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+          `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=fr`
         )
           .then((response) => response.json())
           .then((data) => {
-            console.log(`Weather in ${city}: ${data.weather[0].main}`); // Log updated weather data
+            console.log(`Weather in ${city}: ${data.weather[0].main} `); // Log weather data
+            console.log(`Weather in ${city}: ${data.weather[0].description}`); // decritpion de la meteo
+            console.log(`Weather in ${city}: ${data.main.temp} `); // temperature en kelvin
+            console.log(`Weather in ${city}: ${data.wind.speed}`); // Log updated weather data
           });
+        console.log(`Weather in ${city}: ${data.weather[0].description}`); // Log updated weather data
       }, 3600000); // 3600000 ms = 1 hour
     });
 });
